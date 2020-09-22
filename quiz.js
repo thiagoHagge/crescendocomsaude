efb54c9a0993a3f91d792d42fcaebd2967b7f408
thiagoHagge <thiagohagge09@gmail.com>
2020-09-22 15:35:28 -0300

    big structure commit

---
+++ quiz.js
@@ -0,0 +1,8 @@
+$("#button-gender").click(function() {
+    $("#boy-or-girl").hide();
+    $("#how-old").show();
+})
+$("#button-age").click(function() {
+    $("#how-old").hide();
+    $("#food").show();
+})
